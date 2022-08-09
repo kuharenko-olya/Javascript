9561e01bbad0d297bee59f128207e43c6d4054fc
Kunica Olya <olyavolk49@gmail.com>
2022-08-09 14:13:22 +0300

    add homework 1

---
+++ lessons/task-1.js
@@ -0,0 +1,75 @@
+// 1.
+const myName = 'Olya';
+let name = 'Olya';
+myName = 'Kate';
+name = 'Sasha';
+
+
+//3.
+let numbers = [1, 2, 3, 4, 5];
+let apple = {
+    price: 100,
+    product: 'Apple',
+    delivery: true,
+    sort: 'Golden'
+}
+
+let world = 'мир';
+let greeting = `Привет ${world}`;
+
+
+//5.
+let isAdmin = false;
+let isSuperAdmin = true;
+let isUnauthorized = false;
+
+if (isAdmin) {
+    console.log('Hello admin!');
+} else if (isSuperAdmin) {
+    console.log('Hello superadmin!');
+} else if (isUnauthorized) {
+    console.log('Hello unauthorized ');
+} else {
+    console.log('Hello user!');
+}
+
+
+let role = 'admin';
+
+switch (role) {
+    case 'admin':
+        console.log('Hello admin!');
+        break;
+    case 'superadmin':
+        console.log('Hello superadmin!');
+        break;
+    case 'user':
+        console.log('Hello user!');
+        break;
+    case 'unauthorized':
+        console.log('Hello unauthorized');
+        break;
+    default:
+        console.log('default');
+        break;
+}
+
+const result = (isAdmin) ? 'Hello admin' : (isSuperAdmin) ? 'Hello superadmin' :
+    (isUnauthorized) ? 'Hello unauthorized' : 'Hello user';
+console.log(result);
+
+
+//6.
+let isFruit = 'apple';
+switch (isFruit) {
+    case 'apple':
+    case 'pear':
+    case 'banana':
+    case 'orange':
+    case 'peach':
+        console.log('It is fruit');
+        break;
+    default:
+        console.log('It is vegetable');
+        break;
+}
